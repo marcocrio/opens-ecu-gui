6c90636bc813eb67bebae05971d5129892ab98a8
Marco A Ramirez <maramirez25@miners.utep.edu>
2020-04-27 01:01:42 -0400

    gauge animation

--- main.py
+++ main.py
@@ -3,15 +3,46 @@ from kivy.uix.widget import Widget
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.floatlayout import FloatLayout
-from kivy.properties import NumericProperty
+from kivy.uix.textinput import TextInput
+from kivy.properties import *
+from kivy.animation import Animation
 import kivy.graphics
 
 from math import cos, sin, pi
-
+from functools import partial
 
 
 class DataWindow(GridLayout):
-    pass
+
+
+    def gaugeChange(self, rpm):
+        if rpm:
+            try:
+                self.gauge
+            except Exception:
+                self.cmd.text: "Error"
+
+    def on_enter(self, value):
+        print(value[5:] )
+        self.display.text = "ecu> "
+        if float(value[5:]) > 22.5:
+            #self.level = 22.5
+            anim = Animation(level = 22.5)
+        elif float(value[5:]) < -135:
+            #self.level = -135
+            anim = Animation(level = -135)
+        else:
+            # self.level = value[5:]
+            anim = Animation(level = float(value[5:]))
+
+        anim.start(self)
+
+    #Keeps user from deleting name in console
+    def bashlook(self,value, crs):
+        if(value[:5] != 'ecu> '):
+            self.display.text = "ecu> "
+
+
 
 class OpensecuApp(App):
     def build(self):
@@ -19,5 +50,4 @@ class OpensecuApp(App):
 
 
 if __name__ == '__main__':
-    app = OpensecuApp()
-    app.run()
+    OpensecuApp().run()
